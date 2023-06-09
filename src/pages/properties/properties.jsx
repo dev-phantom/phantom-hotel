2dc5ea01e187f95bd8e01a8c13fff15910c7bea3
dev-phantom <godsfavour1975@gmail.com>
2023-06-10 00:16:28 +0100

    feat: added properties page

--- src/pages/properties/properties.jsx
+++ src/pages/properties/properties.jsx
@@ -1,12 +1,108 @@
 import React from "react";
 import Navbar from "../../common/Navbar";
-export default function Properties() {
+import { Tab } from "@headlessui/react";
+import Properties from "../../components/Properties";
+
+
+export default function Propertie() {
+    function classNames(...classes) {
+      return classes.filter(Boolean).join(" ");
+    }
   return (
     <div className="flex bg-[#222] flex-col min-h-screen">
       <div>
         <Navbar />
       </div>
-      test
+      <>
+        <div>
+          <h2 className="font-interb text-center pl-6 pt-10 lg:text-left xl:text-left capitalize text-[26px] text-white">
+            <span className="border-b-2 pb-2 w-full border-primary">Top</span>{" "}
+            properties
+          </h2>
+        </div>
+
+        <div className="">
+          <Tab.Group>
+            <Tab.Panels>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </>
+      <>
+        <div>
+          <h2 className="font-interb text-center pl-6 pt-10 lg:text-left xl:text-left capitalize text-[26px] text-white">
+            <span className="border-b-2 pb-2 w-full border-primary">friday</span>{" "}
+            special
+          </h2>
+        </div>
+
+        <div className="">
+          <Tab.Group>
+            <Tab.Panels>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </>
+      <>
+        <div>
+          <h2 className="font-interb text-center pl-6 pt-10 lg:text-left xl:text-left capitalize text-[26px] text-white">
+            <span className="border-b-2 pb-2 w-full border-primary">affordable</span>{" "}
+            properties
+          </h2>
+        </div>
+
+        <div className="">
+          <Tab.Group>
+            <Tab.Panels>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+              <Tab.Panel>
+                <Properties />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      </>
     </div>
   );
 }
